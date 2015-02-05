c5b95447e8e7ab4ffb3f12cc2d8b3dcd1b2560cc
Giorgia <giorgia1london@gmail.com>
2015-02-05 12:56:18 +0000

    I LOVE JQUERY

--- frontend/public/jQuery/createMatrix.js
+++ frontend/public/jQuery/createMatrix.js
@@ -1,7 +1,17 @@
-var insertNewRow = function(){
-	var x=document.getElementById('myTable').insertRow();
-	var y = x.insertCell(0);
-	var z = x.insertCell(0);
-	y.innerHTML="";
-	z.innerHTML="";
+var insertRows = function(){
+	myTable.style.color = "blue"
+	for(var i=0; i < 9; i++ ){
+	 var rowNum = "row" + i
+	 rowNum = document.getElementById('myTable').insertRow();
+	 console.log(rowNum);
+	}
+	// var y = x.insertCell(0);
+	// var z = x.insertCell(0);
+	// var f = x.insertCell(0);
+	// var g = x.insertCell(0);
 }
+var insertCells = function(){
+	insertRows()
+	
+}
+insertCells()
