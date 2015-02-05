8e51e89c29ed45fa90792610614df3eac793d65f
Giorgia <giorgia1london@gmail.com>
2015-02-05 13:17:04 +0000

    less for server copiling done

--- api/server.js
+++ api/server.js
@@ -5,8 +5,8 @@ var path = require('path');
 var fs = require('fs');
 var url = require('url');
 var bodyParser = require('body-parser');
+var less = require('less');
 var port = process.env.PORT || 4000
-
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -15,6 +15,11 @@ app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
 app.use(express.static(path.join(__dirname + "./../frontend/public")));
 
+
+
+// less.render('.class { width: (1 + 1) }', function (e, output) {
+//   console.log(output.css);
+// });
 
 app.get('/', function(req, res) {
   res.render('board.html');
